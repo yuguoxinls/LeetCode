f1dbbecaa998f9428eeb92ab9c050d2a2568e24a
Jack <guoxinyu0617@gmail.com>
2022-10-25 14:56:19 +0800

    update

--- SwordOffer/src/Hot100/Solution.java
+++ SwordOffer/src/Hot100/Solution.java
@@ -568,28 +568,30 @@ public class Solution {
      * 请你将两个数相加，并以相同形式返回一个表示和的链表。
      * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
      */
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) { // TODO: 2022/10/25 待修改
-        ListNode falseHead = new ListNode(), tmp = falseHead;
-        boolean flag = false;
-        while (l1 != null && l2 != null){
-            ListNode node;
-            int sum = l1.val + l2.val;
-            if (sum > 9){
-                flag = true;
-                sum = sum - 10;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) { // TODO: 2022/10/25 官方题解
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
             }
-            if (flag){
-                node = new ListNode(sum + 1);
-                flag = false;
-            }else {
-                node = new ListNode(sum);
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
             }
-            tmp.next = node;
-            tmp = tmp.next;
-            l1 = l1.next;
-            l2 = l2.next;
+            if (l2 != null) {
+                l2 = l2.next;
+            }
         }
-
-        return falseHead.next;
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
     }
 }
