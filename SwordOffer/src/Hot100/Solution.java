502df243e7dc90fe53b1ab5bb437ba85e9275f2a
Jack <guoxinyu0617@gmail.com>
2022-10-25 11:10:40 +0800

    update

--- SwordOffer/src/Hot100/Solution.java
+++ SwordOffer/src/Hot100/Solution.java
@@ -562,4 +562,34 @@ public class Solution {
         merged.right = mergeTrees(root1.right, root2.right);
         return merged;
     }
+    //############################################# 中等难度 ##########################################################//
+    /**
+     * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+     * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+     * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) { // TODO: 2022/10/25 待修改
+        ListNode falseHead = new ListNode(), tmp = falseHead;
+        boolean flag = false;
+        while (l1 != null && l2 != null){
+            ListNode node;
+            int sum = l1.val + l2.val;
+            if (sum > 9){
+                flag = true;
+                sum = sum - 10;
+            }
+            if (flag){
+                node = new ListNode(sum + 1);
+                flag = false;
+            }else {
+                node = new ListNode(sum);
+            }
+            tmp.next = node;
+            tmp = tmp.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        return falseHead.next;
+    }
 }
