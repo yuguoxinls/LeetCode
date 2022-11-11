532af3fd911a632e05500e1eb70d570aaed414d9
Jack <guoxinyu0617@gmail.com>
2022-11-11 14:40:58 +0800

    update

--- SwordOffer/src/LeetCodeByClass/Algorithm/Search/SolutionTest.java
+++ SwordOffer/src/LeetCodeByClass/Algorithm/Search/SolutionTest.java
@@ -27,4 +27,10 @@ public class SolutionTest {
         char[][] grid = {{'1','1','1','1','0'},{'1','1','0','1','0'},{'1','1','0','0','0'},{'0','0','0','0','0'}};
         System.out.println(solution.numIslands(grid));
     }
+
+    @Test
+    public void findCircleNum() {
+        int[][] grid = {{1,0,0},{0,1,0},{0,0,1}};
+        System.out.println(solution.findCircleNum(grid));
+    }
 }
