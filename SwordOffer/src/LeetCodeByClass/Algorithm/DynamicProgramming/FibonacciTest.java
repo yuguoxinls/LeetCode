80a58150fa6c1b1508a121827faa53b6804fb27d
Jack <guoxinyu0617@gmail.com>
2022-11-16 17:18:15 +0800

    update

---
+++ SwordOffer/src/LeetCodeByClass/Algorithm/DynamicProgramming/FibonacciTest.java
@@ -0,0 +1,25 @@
+package LeetCodeByClass.Algorithm.DynamicProgramming;
+
+
+import org.testng.annotations.Test;
+
+public class FibonacciTest {
+    Fibonacci fibonacci = new Fibonacci();
+
+    @Test
+    public void climbStairs() {
+        System.out.println(fibonacci.climbStairs(2));
+    }
+
+    @Test
+    public void rob() {
+        int[] nums = {1,2};
+        System.out.println(fibonacci.rob(nums));
+    }
+
+    @Test
+    public void rob2() {
+        int[] nums = {1,2,3,1};
+        System.out.println(fibonacci.rob2(nums));
+    }
+}
