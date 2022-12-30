23f69d82ddee6a421ba5f001252af52a4e176bb5
Jack <guoxinyu0617@gmail.com>
2022-12-30 10:45:41 +0800

    update

---
+++ SwordOffer/src/test/LeetCodeByClass/DataStructure/BitOperations/SolutionTest.java
@@ -0,0 +1,46 @@
+package test.LeetCodeByClass.DataStructure.BitOperations; 
+
+import LeetCodeByClass.DataStructure.BitOperations.Solution;
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After;
+
+import java.util.Arrays;
+
+/** 
+* Solution Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>12ÔÂ 30, 2022</pre> 
+* @version 1.0 
+*/ 
+public class SolutionTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: hammingDistance(int x, int y) 
+* 
+*/
+Solution solution = new Solution();
+@Test
+public void testHammingDistance() throws Exception { 
+//TODO: Test goes here...
+
+    System.out.println(solution.hammingDistance(1, 4));
+}
+
+@Test
+public void testSingleNumber3(){
+    System.out.println(Arrays.toString(solution.singleNumber3(new int[]{1, 2, 1, 3, 2, 5})));
+}
+
+
+} 
