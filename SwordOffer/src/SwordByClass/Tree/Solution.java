cc879ad28e2b64816016a74adc6bb73d35fb3d26
Jack <guoxinyu0617@gmail.com>
2023-01-06 15:59:56 +0800

    update

--- SwordOffer/src/SwordByClass/Tree/Solution.java
+++ SwordOffer/src/SwordByClass/Tree/Solution.java
@@ -67,8 +67,73 @@ public class Solution {
      * 输入：A = [3,4,5,1,2], B = [4,1]
      * 输出：true
      */
-//    public boolean isSubStructure(TreeNode A, TreeNode B) {
-//
-//    }
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        // TODO: 2023/1/6
+        // B是A的子结构，那么A的任意一个节点都有可能是B的根节点，因此进行下面2个步骤：
+        // 1. 遍历A的每一个节点 -> isSubStructure()
+        // 2. 针对遍历到的每个节点，判断以该节点为根节点的子树是否包含树B -> isContain()
+
+        // 2个树都不能是null，并且若B是A的子结构，必然满足以下3个条件之一：
+        // 1. 以 节点 A 为根节点的子树 包含树 B
+        // 2. 树B是树A左子树的子结构
+        // 3. 。。。。右子树的子结构    2，3两步相当于在对A做先序遍历
+        return (A != null && B != null) && (isContain(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+    }
+    private boolean isContain(TreeNode A, TreeNode B) { // 判断以A为根节点的树是否包含树B
+        if (B == null) return true; // B为空，说明树 B 已匹配完成（越过叶子节点），因此返回true
+        if (A == null || A.val != B.val) return false; // 值不相等，肯定返回false
+        return isContain(A.left, B.left) && isContain(A.right, B.right); // 要包含，左右子树也都要满足包含条件
+    }
+
+    /**
+     * 27. 二叉树的镜像
+     * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+     * 例如输入：
+     *      4
+     *    /   \
+     *   2     7
+     *  / \   / \
+     * 1   3 6   9
+     * 镜像输出：
+     *      4
+     *    /   \
+     *   7     2
+     *  / \   / \
+     * 9   6 3   1
+     */
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode right = root.right;
+        root.right = mirrorTree(root.left);
+        root.left = mirrorTree(right);
+        return root;
+    }
+
+    /**
+     * 28. 对称的二叉树
+     * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+     * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+     *     1
+     *    / \
+     *   2   2
+     *  / \ / \
+     * 3  4 4  3
+     * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+     *     1
+     *    / \
+     *   2   2
+     *    \   \
+     *    3    3
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
 
 }
