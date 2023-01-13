bb173ce3430627845319e3c507b9b98bf821ee1d
Jack <guoxinyu0617@gmail.com>
2023-01-13 11:41:31 +0800

    update

---
+++ SwordOffer/src/test/SwordByClass/DynamicProgramming/SolutionTest.java
@@ -0,0 +1,58 @@
+package test.SwordByClass.DynamicProgramming; 
+
+import SwordByClass.DynamicProgramming.Solution;
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Solution Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>1ÔÂ 13, 2023</pre> 
+* @version 1.0 
+*/ 
+public class SolutionTest {
+    Solution solution = new Solution();
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: fib(int n) 
+* 
+*/ 
+@Test
+public void testFib() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: numWays(int n) 
+* 
+*/ 
+@Test
+public void testNumWays() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: maxSubArray(int[] nums) 
+* 
+*/ 
+@Test
+public void testMaxSubArray() throws Exception { 
+//TODO: Test goes here...
+    System.out.println(solution.maxSubArray(new int[]{-1,-2}));
+} 
+
+
+} 
